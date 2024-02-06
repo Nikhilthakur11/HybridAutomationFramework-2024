250883f5d64f51b5f211047c0a7b89c91b0feea8
Thakur Nikhil Singh <thakur36nikhil@gmail.com>
2024-02-06 16:43:16 +0530

    udpated pom and other files

--- src/test/java/example/example/tests/FaceBookLoginTest.java
+++ src/test/java/example/example/tests/FaceBookLoginTest.java
@@ -3,6 +3,7 @@ package example.example.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import example.example.DataDriven.ExcelDataProvider;
 import example.example.factory.PageinstancesFactory;
 import example.example.pages.FacebookLoginPage;
 
@@ -13,12 +14,13 @@ import example.example.pages.FacebookLoginPage;
  */
 @Test(testName = "Facebook login test", description = "Facebook login test")
 public class FaceBookLoginTest extends BaseTest {
+	ExcelDataProvider dr=new ExcelDataProvider();
 
-	@Test
-	public void facebookLoginTest() {
+	@Test(dataProvider="excelData",dataProviderClass=ExcelDataProvider.class)
+	public void facebookLoginTest(String email,String pwd) {
 		driver.get("https://www.facebook.com/");
 		FacebookLoginPage facebookLoginPage = PageinstancesFactory.getInstance(FacebookLoginPage.class);
-		facebookLoginPage.enterEmail("abc").enterPassword("abc").clickSignIn();
+		facebookLoginPage.enterEmail(email).enterPassword(pwd).clickSignIn();
 		Assert.assertTrue(false, "Login failed : Test failed");
 	}
 }
